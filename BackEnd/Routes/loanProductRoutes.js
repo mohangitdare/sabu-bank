51d5e812e0d9538583af857e8d90c503ffc5b961
S <2387921@cognizant.com>
2025-05-01 15:46:07 +0530

    uploaded

--- BackEnd/Routes/loanProductRoutes.js
+++ BackEnd/Routes/loanProductRoutes.js
@@ -1,9 +1,23 @@
+// const express = require("express")
+// const router = express.Router();
+// const loanProductController =  require("./../Controllers/loanProductController");
+
+// router.route("/").get(loanProductController.getAllLoanProducts).post(loanProductController.createLoanProduct)
+
+// router.route("/:id").get(loanProductController.getLoanProductById).delete(loanProductController.deleteLoanProductById).patch(loanProductController.updateLoanProductById)
+
+// module.exports = router;
 const express = require("express")
 const router = express.Router();
 const loanProductController =  require("./../Controllers/loanProductController");
 
-router.route("/").get(loanProductController.getAllLoanProducts).post(loanProductController.createLoanProduct)
+router.route("/")
+  .get(loanProductController.getAllLoanProducts)
+  .post(loanProductController.createLoanProduct);
 
-router.route("/:id").get(loanProductController.getLoanProductById).delete(loanProductController.deleteLoanProductById).patch(loanProductController.updateLoanProductById)
+router.route("/:id")
+  .get(loanProductController.getLoanProductById)
+  .delete(loanProductController.deleteLoanProductById)
+  .patch(loanProductController.updateLoanProductById);
 
 module.exports = router;
--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,10 +1,20 @@
+// const express = require("express")
+// const app = express();
+// const loanProductRoutes = require("./Routes/loanProductRoutes") 
+// const cors = require("cors")
+
+// app.use(cors());
+// app.use(express.json());
+// app.use("/api/v1/loan-products",loanProductRoutes);
+
+// module.exports = app;
 const express = require("express")
 const app = express();
-const loanProductRoutes = require("./Routes/loanProductRoutes") 
+const loanProductRoutes = require("./Routes/loanProductRoutes")
 const cors = require("cors")
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/v1/loan-products",loanProductRoutes);
+app.use("/api/v1/loan-products", loanProductRoutes);
 
 module.exports = app;
